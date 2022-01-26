4a019e9cc298a3f57ad15559ae9a971b066458ea
RichoM <richi.moran@gmail.com>
2022-01-26 00:25:21 -0300

    Moved board definitions into its own namespace

---
+++ src/teg_online/board.cljs
@@ -0,0 +1,36 @@
+(ns teg-online.board)
+
+(def continents {::asia {:name "Asia" :bonus 7}
+                 ::europa {:name "Europa" :bonus 5}
+                 ::north-america {:name "América del Norte" :bonus 5}
+                 ::south-america {:name "América del Sur" :bonus 3}
+                 ::africa {:name "África" :bonus 3}
+                 ::oceania {:name "Oceanía" :bonus 2}})
+
+(def countries {::argentina {:name "Argentina"
+                             :continent ::south-america
+                             :neighbours #{::peru ::brasil ::uruguay ::chile}}
+                ::chile {:name "Chile"
+                         :continent ::south-america
+                         :neighbours #{::peru ::argentina}}
+                ::peru {:name "Perú"
+                        :continent ::south-america
+                        :neighbours #{::colombia ::brasil ::argentina ::chile}}
+                ::brasil {:name "Brasil"
+                          :continent ::south-america
+                          :neighbours #{::colombia ::peru ::argentina ::uruguay}}
+                ::uruguay {:name "Uruguay"
+                           :continent ::south-america
+                           :neighbours #{::argentina ::brasil}}
+                ::colombia {:name "Colombia"
+                            :continent ::south-america
+                            :neighbours #{::peru ::brasil}}})
+
+(def card-symbols #{::balloon ::cannon ::ship ::all})
+
+(def cards #{[::argentina ::all]
+             [::chile ::balloon]
+             [::peru ::ship]
+             [::brasil ::ship]
+             [::uruguay ::balloon]
+             [::colombia ::balloon]})
