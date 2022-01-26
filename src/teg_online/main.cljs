e7b46e60a3af219268b102454d4f51eccb89ef5f
RichoM <richi.moran@gmail.com>
2022-01-26 00:25:55 -0300

    Starting to implement functions to setup the game

--- src/teg_online/main.cljs
+++ src/teg_online/main.cljs
@@ -1,7 +1,41 @@
-(ns teg-online.main)
+(ns teg-online.main
+  (:require [teg-online.board :as board]
+            [teg-online.utils :as u]))
 
+(defn new-game []
+  {:players []})
 
+(defn new-player [id name]
+  {:id id
+   :name name
+   :cards #{}
+   :army {}})
 
+(defn join-game [game player]
+  (update game :players conj player))
+
+(defn distribute-countries [game]
+  (assoc game :players
+         (mapv (fn [[player countries]]
+                 (assoc player
+                        :army (into {}
+                                    (mapv (fn [c] [c 1])
+                                          countries))))
+               (u/deal (keys board/countries) (game :players)))))
 
 (defn init []
   (print "RICHO!"))
+
+(comment
+
+  (u/deal (range 13) "ABC")
+
+  (def game @game)
+  (def game (atom (new-game)))
+  (swap! game join-game (new-player ::p1 "Richo"))
+  (swap! game join-game (new-player ::p2 "Lechu"))
+  (swap! game join-game (new-player ::p3 "Diego"))
+  (swap! game distribute-countries)
+
+  (reduce + (-> @game :players (nth 0) :army vals))
+  )
