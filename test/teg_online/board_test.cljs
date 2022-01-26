d12da510954b6e9354dacbe40c2e2fbac6bc31f9
RichoM <richi.moran@gmail.com>
2022-01-26 18:42:03 -0300

    Added a few simple tests for the board data

---
+++ test/teg_online/board_test.cljs
@@ -0,0 +1,22 @@
+(ns teg-online.board-test
+  (:require [cljs.test :refer-macros [deftest is testing use-fixtures]]
+            [teg-online.board :as b]))
+
+
+(deftest sanity-check
+  (is (= 1 1)))
+
+(deftest countries-should-have-a-valid-continent
+  (doseq [[id {:keys [name continent]}] b/countries]
+    (is (contains? b/continents continent)
+        (str name " (" id ") has an invalid continent (" continent ")"))))
+
+(deftest countries-should-have-valid-neighbours
+  (doseq [[id {:keys [name neighbours]}] b/countries]
+    (is (seq neighbours)
+        (str name " (" id ") should have at least one neighbour"))
+    (doseq [neighbour neighbours]
+      (is (not= id neighbour)
+          (str name " (" id ") cannot be its own neighbour"))
+      (is (contains? b/countries neighbour)
+          (str name " (" id ") has an invalid neighbour (" neighbour ")")))))
