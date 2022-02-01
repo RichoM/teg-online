0478b4e6a68ec0b00fd0f5c9e0af9dd9488fedb9
RichoM <richi.moran@gmail.com>
2022-02-01 15:02:51 -0300

    Added all countries

--- src/teg_online/board.cljs
+++ src/teg_online/board.cljs
@@ -7,19 +7,19 @@
                  ::africa {:name "África" :bonus 3}
                  ::oceania {:name "Oceanía" :bonus 2}})
 
-(def countries {; South América
+(def countries {; South America
                 ::argentina {:name "Argentina"
                              :continent ::south-america
                              :neighbours #{::peru ::brasil ::uruguay ::chile}}
                 ::chile {:name "Chile"
                          :continent ::south-america
-                         :neighbours #{::peru ::argentina}}
+                         :neighbours #{::peru ::argentina ::australia}}
                 ::peru {:name "Perú"
                         :continent ::south-america
                         :neighbours #{::colombia ::brasil ::argentina ::chile}}
                 ::brasil {:name "Brasil"
                           :continent ::south-america
-                          :neighbours #{::colombia ::peru ::argentina ::uruguay}}
+                          :neighbours #{::colombia ::peru ::argentina ::uruguay ::sahara}}
                 ::uruguay {:name "Uruguay"
                            :continent ::south-america
                            :neighbours #{::argentina ::brasil}}
@@ -27,10 +27,10 @@
                             :continent ::south-america
                             :neighbours #{::peru ::brasil ::mexico}}
 
-                ; North América
+                ; North America
                 ::alaska {:name "Alaska"
                           :continent ::north-america
-                          :neighbours #{::yukon ::oregon}}
+                          :neighbours #{::yukon ::oregon ::katchatka}}
                 ::yukon {:name "Yukón"
                          :continent ::north-america
                          :neighbours #{::alaska ::canada ::oregon}}
@@ -51,13 +51,123 @@
                             :neighbours #{::terranova ::groenlandia}}
                 ::groenlandia {:name "Groenlandia"
                                :continent ::north-america
-                               :neighbours #{::labrador ::nueva-york}}
+                               :neighbours #{::labrador ::nueva-york ::islandia}}
                 ::california {:name "California"
                               :continent ::north-america
                               :neighbours #{::oregon ::nueva-york ::mexico}}
                 ::mexico {:name "México"
                           :continent ::north-america
-                          :neighbours #{::california ::colombia}}})
+                          :neighbours #{::california ::colombia}}
+
+                ; Africa
+                ::sahara {:name "Sahara"
+                          :continent ::africa
+                          :neighbours #{::zaire ::etiopia ::egipto ::brasil ::españa}}
+                ::zaire {:name "Zaire"
+                         :continent ::africa
+                         :neighbours #{::sahara ::etiopia ::sudafrica ::madagascar}}
+                ::etiopia {:name "Etiopía"
+                           :continent ::africa
+                           :neighbours #{::sahara ::egipto ::sudafrica ::zaire}}
+                ::sudafrica {:name "Sudáfrica"
+                             :continent ::africa
+                             :neighbours #{::zaire ::etiopia}}
+                ::egipto {:name "Egipto"
+                          :continent ::africa
+                          :neighbours #{::sahara ::etiopia ::madagascar ::polonia ::israel}}
+                ::madagascar {:name "Madagascar"
+                              :continent ::africa
+                              :neighbours #{::egipto ::zaire}}
+
+                ; Europa
+                ::islandia {:name "Islandia"
+                            :continent ::europa
+                            :neighbours #{::groenlandia ::suecia ::gran-bretaña}}
+                ::gran-bretaña {:name "Gran Bretaña"
+                                :continent ::europa
+                                :neighbours #{::islandia ::españa ::alemania}}
+                ::suecia {:name "Suecia"
+                          :continent ::europa
+                          :neighbours #{::islandia ::rusia}}
+                ::rusia {:name "Rusia"
+                         :continent ::europa
+                         :neighbours #{::suecia ::polonia ::aral ::iran ::turquia}}
+                ::polonia {:name "Polonia"
+                           :continent ::europa
+                           :neighbours #{::alemania ::egipto ::rusia ::turquia}}
+                ::alemania {:name "Alemania"
+                            :continent ::europa
+                            :neighbours #{::gran-bretaña ::francia ::italia ::polonia}}
+                ::italia {:name "Italia"
+                          :continent ::europa
+                          :neighbours #{::francia ::alemania}}
+                ::francia {:name "Francia"
+                           :continent ::europa
+                           :neighbours #{::españa ::alemania ::italia}}
+                ::españa {:name "España"
+                          :continent ::europa
+                          :neighbours #{::gran-bretaña ::sahara ::francia}}
+
+                ; Asia
+                ::aral {:name "Aral"
+                        :continent ::asia
+                        :neighbours #{::rusia ::tartaria ::siberia ::iran ::mongolia}}
+                ::tartaria {:name "Tartaria"
+                            :continent ::asia
+                            :neighbours #{::aral ::siberia ::taimir}}
+                ::taimir {:name "Taimir"
+                          :continent ::asia
+                          :neighbours #{::tartaria ::siberia}}
+                ::siberia {:name "Siberia"
+                           :continent ::asia
+                           :neighbours #{::aral ::tartaria ::taimir ::katchatka ::china ::mongolia}}
+                ::katchatka {:name "Katchatka"
+                             :continent ::asia
+                             :neighbours #{::alaska ::japon ::china ::siberia}}
+                ::iran {:name "Irán"
+                        :continent ::asia
+                        :neighbours #{::aral ::mongolia ::gobi ::china ::india ::turquia ::rusia}}
+                ::mongolia {:name "Mongolia"
+                            :continent ::asia
+                            :neighbours #{::aral ::siberia ::china ::gobi ::iran}}
+                ::china {:name "China"
+                         :continent ::asia
+                         :neighbours #{::siberia ::katchatka ::japon ::malasia ::india ::iran ::gobi ::mongolia}}
+                ::japon {:name "Japón"
+                         :continent ::asia
+                         :neighbours #{::katchatka ::china}}
+                ::gobi {:name "Gobi"
+                        :continent ::asia
+                        :neighbours #{::iran ::mongolia ::china}}
+                ::india {:name "India"
+                         :continent ::asia
+                         :neighbours #{::iran ::china ::malasia ::sumatra}}
+                ::malasia {:name "Malasia"
+                           :continent ::asia
+                           :neighbours #{::india ::china ::borneo}}
+                ::turquia {:name "Turquía"
+                           :continent ::asia
+                           :neighbours #{::polonia ::rusia ::iran ::arabia ::israel}}
+                ::israel {:name "Israel"
+                          :continent ::asia
+                          :neighbours #{::turquia ::arabia ::egipto}}
+                ::arabia {:name "Arabia"
+                          :continent ::asia
+                          :neighbours #{::israel ::turquia}}
+
+                ; Oceanía
+                ::sumatra {:name "Sumatra"
+                           :continent ::oceania
+                           :neighbours #{::india ::australia}}
+                ::borneo {:name "Borneo"
+                          :continent ::oceania
+                          :neighbours #{::malasia ::australia}}
+                ::java {:name "Java"
+                        :continent ::oceania
+                        :neighbours #{::australia}}
+                ::australia {:name "Australia"
+                             :continent ::oceania
+                             :neighbours #{::sumatra ::borneo ::java ::chile}}})
 
 (def card-symbols #{::balloon ::cannon ::ship ::all})
 
