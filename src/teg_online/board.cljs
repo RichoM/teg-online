00132e5e47d937c4c1f78b5f293f80905efd427c
RichoM <richi.moran@gmail.com>
2022-02-01 14:21:08 -0300

    Adding more data about the board

--- src/teg_online/board.cljs
+++ src/teg_online/board.cljs
@@ -7,7 +7,8 @@
                  ::africa {:name "África" :bonus 3}
                  ::oceania {:name "Oceanía" :bonus 2}})
 
-(def countries {::argentina {:name "Argentina"
+(def countries {; South América
+                ::argentina {:name "Argentina"
                              :continent ::south-america
                              :neighbours #{::peru ::brasil ::uruguay ::chile}}
                 ::chile {:name "Chile"
@@ -24,7 +25,39 @@
                            :neighbours #{::argentina ::brasil}}
                 ::colombia {:name "Colombia"
                             :continent ::south-america
-                            :neighbours #{::peru ::brasil}}})
+                            :neighbours #{::peru ::brasil ::mexico}}
+
+                ; North América
+                ::alaska {:name "Alaska"
+                          :continent ::north-america
+                          :neighbours #{::yukon ::oregon}}
+                ::yukon {:name "Yukón"
+                         :continent ::north-america
+                         :neighbours #{::alaska ::canada ::oregon}}
+                ::canada {:name "Canadá"
+                          :continent ::north-america
+                          :neighbours #{::yukon ::oregon ::nueva-york ::terranova}}
+                ::oregon {:name "Oregon"
+                          :continent ::north-america
+                          :neighbours #{::alaska ::yukon ::canada ::nueva-york ::california}}
+                ::nueva-york {:name "Nueva York"
+                              :continent ::north-america
+                              :neighbours #{::canada ::terranova ::groenlandia ::california ::oregon}}
+                ::terranova {:name "Terranova"
+                             :continent ::north-america
+                             :neighbours #{::canada ::labrador ::nueva-york}}
+                ::labrador {:name "Labrador"
+                            :continent ::north-america
+                            :neighbours #{::terranova ::groenlandia}}
+                ::groenlandia {:name "Groenlandia"
+                               :continent ::north-america
+                               :neighbours #{::labrador ::nueva-york}}
+                ::california {:name "California"
+                              :continent ::north-america
+                              :neighbours #{::oregon ::nueva-york ::mexico}}
+                ::mexico {:name "México"
+                          :continent ::north-america
+                          :neighbours #{::california ::colombia}}})
 
 (def card-symbols #{::balloon ::cannon ::ship ::all})
 
