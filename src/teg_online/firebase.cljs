be0e7b4c52bcb1ed8e1e3515c53cafa6272c052e
RichoM <richi.moran@gmail.com>
2022-03-15 14:05:13 -0300

    Updated firebase game <-> doc transformation functions

--- src/teg_online/firebase.cljs
+++ src/teg_online/firebase.cljs
@@ -12,23 +12,32 @@
 (def destructors (atom []))
 
 (defn game->doc [game]
-  (assoc (select-keys game [:phase :turn :turn-order :players :winner])
+  (assoc (select-keys game [:phase :turn :turn-order :players :winner :current-turn])
+         :cards (mapv (fn [country]
+                        (let [{:keys [owner type]} (get-in game [:cards country])]
+                          {:owner owner, :type type}))
+                      sorted-countries)
          :countries (mapv (fn [country]
                             (let [{:keys [army owner]} (get-in game [:countries country])]
                               {:army army, :owner owner}))
                           sorted-countries)))
 
-(defn doc->game [{:keys [phase turn turn-order players countries winner]}]
+(defn doc->game [{:keys [phase turn turn-order players countries winner cards current-turn]}]
   {:phase (when phase (keyword (namespace ::teg/*) phase))
    :turn turn
    :turn-order (mapv keyword turn-order)
+   :current-turn current-turn
    :winner (when winner (keyword winner))
-   :players (into {} (map (fn [[id {:keys [name goal playing?]}]]
-                            [id {:id id
-                                 :goal goal
-                                 :name name
-                                 :playing? playing?}])
+   :players (into {} (map (fn [[id player]]
+                            [id (assoc player :id id)])
                           players))
+   :cards (->> cards
+               (map-indexed (fn [idx {:keys [type owner]}]
+                              (let [country (nth sorted-countries idx)]
+                                [country {:country country
+                                          :type (keyword (namespace ::b/*) type)
+                                          :owner (keyword owner)}])))
+               (into {}))
    :countries (into {} (map-indexed (fn [idx {:keys [army owner]}]
                                       (let [id (nth sorted-countries idx)]
                                         [id {:id id
