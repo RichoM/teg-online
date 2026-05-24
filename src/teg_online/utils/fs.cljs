7730a52a1908a9afe60c939eda2f94ab84f956f2
RichoM <richi.moran@gmail.com>
2026-05-24 12:06:00 -0300

    Added filesystem utilities

---
+++ src/teg_online/utils/fs.cljs
@@ -0,0 +1,29 @@
+(ns teg-online.utils.fs
+  (:refer-clojure :exclude [exists?])
+  (:require [clojure.core.async :as a :refer [go <!]]
+            ["fs" :as fs]))
+
+(defn callback [p]
+  (fn [err]
+    (if err
+      (a/put! p err)
+      (a/close! p))))
+
+(defn mkdir [path]
+  (let [result (a/promise-chan)]
+    (fs/mkdir path (callback result))
+    result))
+
+(defn write-file 
+  ([path data] (write-file path data {}))
+  ([path data options]
+  (let [result (a/promise-chan)]
+    (fs/writeFile path data 
+                  (clj->js options) 
+                  (callback result))
+    result)))
+
+(defn exists? [file]
+  (let [result (a/promise-chan)]
+    (fs/access file (fn [err] (a/put! result (nil? err))))
+    result))
