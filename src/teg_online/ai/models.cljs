a9379da931d41227615dbcb898b9ec289d8bc649
RichoM <richi.moran@gmail.com>
2026-08-27 11:09:40 -0300

    Refactoring. Moved model handlers into their own namespace

--- src/teg_online/ai/models.cljs
+++ src/teg_online/ai/models.cljs
@@ -1,6 +1,5 @@
-(ns teg-online.ai.models 
-  (:require
-    [teg-online.utils.core :as u]))
+(ns teg-online.ai.models
+  (:require [teg-online.utils.core :as u]))
 
 (def models (->> [{:id :random
                    :name "random"}
@@ -8,50 +7,25 @@
                    :name "best"}
                   {:id :gpt-4.1-mini/basic_t00
                    :name "gpt-4.1-mini"
-                   :basic? true
                    :temperature 0
                    :display-name "gpt-4.1-mini (basic, t: 0)"}
-                  #_{:id :gpt-4.1-mini/basic_t04
-                     :name "gpt-4.1-mini"
-                     :basic? true
-                     :temperature 0.4
-                     :display-name "gpt-4.1-mini (basic, t: 0.4)"}
                   {:id :gpt-4.1-mini/basic_t05
                    :name "gpt-4.1-mini"
-                   :basic? true
                    :temperature 0.5
                    :display-name "gpt-4.1-mini (basic, t: 0.5)"}
-                  #_{:id :gpt-4.1-mini/basic_t07
-                     :name "gpt-4.1-mini"
-                     :basic? true
-                     :temperature 0.7
-                     :display-name "gpt-4.1-mini (basic, t: 0.7)"}
                   {:id :gpt-4.1-mini/basic_t10
                    :name "gpt-4.1-mini"
-                   :basic? true
                    :temperature 1.0
                    :display-name "gpt-4.1-mini (basic, t: 1)"}
                   {:id :gpt-4.1-mini/strat_t00
                    :name "gpt-4.1-mini"
-                   :basic? false
                    :temperature 0}
-                  #_{:id :gpt-4.1-mini/strat_t04
-                     :name "gpt-4.1-mini"
-                     :basic? false
-                     :temperature 0.4
-                     :display-name "gpt-4.1-mini (t: 0.4)"}
                   {:id :gpt-4.1-mini/strat_t05
                    :name "gpt-4.1-mini"
-                   :basic? false
                    :temperature 0.5
                    :display-name "gpt-4.1-mini (t: 0.5)"}
-                  #_{:id :gpt-4.1-mini/strat_t07
-                     :basic? false
-                     :temperature 0.7
-                     :name "gpt-4.1-mini (t: 0.7)"}
                   {:id :gpt-4.1-mini/strat_t10
                    :name "gpt-4.1-mini"
-                   :basic? false
                    :temperature 1.0
                    :display-name "gpt-4.1-mini (t: 1)"}
                   {:id :gpt-5.4-mini
--- src/teg_online/ai/server.cljs
+++ src/teg_online/ai/server.cljs
@@ -7,16 +7,10 @@
             ["cors" :as cors]
             [cognitect.transit :as t]
             [teg-online.game :as teg]
-            [clojure.pprint :as pp]
+            [teg-online.ai.model-handlers :refer [handler-by-id]]
             [teg-online.ai.attack-prob :refer [initialize-chances]]
-            [teg-online.utils.openai :as ai]
-            [teg-online.ai.basic-prompt :refer [make-prompt]]
-            [teg-online.ai.strategy-prompt :refer [strategy-prompt action-prompt]]
-            [teg-online.ai.response :as response]
             [teg-online.ai.models :refer [models-by-id]]
-            [teg-online.ai.actions :as actions]
-            [teg-online.utils.fs :as fs]
-            [clojure.string :as str]))
+            [teg-online.utils.fs :as fs]))
 
 (defonce server (atom nil))
 
@@ -39,161 +33,15 @@
 (defn write-response [response]
   (t/write writer response))
 
-(defn fetch-response [model prompt]
-  (let [model-name (:name model)
-        temperature (:temperature model)]
-    (ai/create-response!
-     (cond-> {:model model-name
-              :input prompt}
-       ; Only send the temperature if the model specifies it
-       (some? temperature)
-       (assoc :temperature temperature)))))
-
-(defn get-basic-actions! [game turn-actions model log]
-  (go-try
-    (let [prompt (make-prompt game turn-actions)
-          response (when prompt
-                     (:output_text
-                      (<? (fetch-response model prompt))))
-          actions (if response
-                    (response/parse-response game
-                                             turn-actions
-                                             response)
-                    [actions/pass])]
-      (<? (log (str "Basic action\n"
-                    (:turn game) ". " (:phase game)
-                    " / "
-                    (teg/get-current-player-name game))))
-      (<? (log (str ">>> PROMPT:\n" prompt)))
-      (<? (log (str ">>> RESPONSE:\n" response)))
-      (<? (log actions))
-      {:actions actions
-       :conversation [(str "Prompt\n" prompt) 
-                      (str "Response\n" response)]})))
-
-(defn get-strategy! [game turn-actions model log]
-  (go-try
-    (let [initial-prompt (strategy-prompt game turn-actions)
-          strat-response (when initial-prompt
-                           (:output_text
-                            (<? (fetch-response model initial-prompt))))
-          second-prompt (action-prompt game strat-response)
-          action-response (when strat-response
-                            (:output_text
-                             (<? (ai/create-response!
-                                  {:model "gpt-4.1-mini"
-                                   :temperature 0
-                                   :input second-prompt}))))
-          actions (if action-response
-                    (response/parse-response game
-                                             turn-actions
-                                             action-response)
-                    [actions/pass])]
-      (<? (log (str "Strategy\n"
-                    (:turn game) ". " (:phase game)
-                    " / "
-                    (teg/get-current-player-name game))))
-      (when initial-prompt
-        (<? (log (str ">>> INITIAL PROMPT:\n" initial-prompt))))
-      (when strat-response
-        (<? (log (str ">>> STRATEGY:\n" strat-response))))
-      (when second-prompt
-        (<? (log (str ">>> SECOND PROMPT:\n" second-prompt))))
-      (when action-response
-        (<? (log (str ">>> ACTION:\n" action-response))))
-      (when actions
-        (<? (log actions)))
-      {:conversation [(str "Prompt #1\n" initial-prompt)
-                      (str "Response #1\n" strat-response)
-                      (str "Prompt #2\n" second-prompt)
-                      (str "Response #2\n" action-response)]
-       :actions actions})))
-
-(defn get-random-actions! [game turn-actions model log]
-  (go-try
-   (let [player-id (teg/get-current-player game)
-         actions (->> (repeatedly #(actions/random-actions game turn-actions))
-                      (take 1000)
-                      (vec)
-                      (rand-nth))
-         score (get (actions/calculate-score
-                     game
-                     (actions/make-mutation actions))
-                    player-id)]
-     {:conversation []
-      :actions (if (pos? (:absolute score))
-                 actions
-                 [actions/pass])})))
-
-(defn get-best-actions! [game turn-actions model log]
-  (go-try
-   (let [player-id (teg/get-current-player game)
-         all-actions (->> (actions/all-actions game turn-actions)
-                          (take 1000)
-                          (mapv (fn [actions]
-                                  (let [mutation (actions/make-mutation actions)
-                                        score (actions/calculate-score game mutation)]
-                                    {:actions actions
-                                     :score (-> score player-id)}))))
-         sort-normalized (delay
-                           (->> all-actions
-                                (sort-by (comp :normalized :score) >)))
-         sort-absolute (delay
-                         (->> all-actions
-                              (sort-by (comp :absolute :score) >)))
-         best-normalized (delay
-                           (first @sort-normalized))
-         best-absolute (delay
-                         (first @sort-absolute))
-         best (if (and (pos? (-> @best-normalized :score :normalized))
-                       (pos? (-> @best-normalized :score :absolute)))
-                @best-normalized
-                @best-absolute)]
-     (println "ACTION:" best)
-     {:conversation [(str "Options (" (count all-actions) ")"
-                          "\n\n"
-                          (->> @sort-normalized
-                               (map (fn [scored-action]
-                                      (pp/write scored-action :stream nil)))
-                               (str/join "\n\n")))]
-      :actions (if (> (-> best :score :absolute) 0.0)
-                 (:actions best)
-                 [actions/pass])})))
-
-(comment
-  
-  (do
-    (def game (-> @teg-online.main/state :game))
-    (def player-id (teg/get-current-player game)))
-  )
-
-(defn get-placeholder-actions! [game turn-actions model log]
-  (go-try
-   ;(<? (a/timeout (* 1000 (+ 2 (rand-int 10)))))
-   {:conversation [(str "Prompt #1" "\n" (strategy-prompt game turn-actions))
-                   "Response #1\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dictum eros eu felis rhoncus lacinia. Fusce ullamcorper diam sit amet lacus molestie, aliquam sodales felis hendrerit. Nunc iaculis nulla et convallis volutpat. Duis risus libero, dignissim at leo at, interdum rutrum tortor. Vivamus ac rhoncus sapien. Ut lobortis tristique eleifend. Morbi malesuada, ex eu pretium maximus, libero nulla condimentum magna, nec accumsan diam felis ac tortor. Nulla at justo orci. Donec at convallis arcu, quis pellentesque orci. Duis ornare erat leo, eget bibendum nulla interdum quis. Donec viverra, risus at lacinia congue, odio mauris fringilla mauris, ac faucibus lacus nulla eget massa.
-  
-  Aenean mattis facilisis urna, sit amet pharetra augue ullamcorper at. Aenean maximus velit purus, quis facilisis erat vulputate pharetra. Aliquam erat volutpat. Praesent arcu metus, rhoncus vitae urna non, vulputate eleifend metus. Nunc tristique, risus nec fermentum efficitur, ex leo consectetur metus, sit amet posuere neque turpis id lectus. Nullam feugiat volutpat egestas. Quisque quis augue aliquam, volutpat lorem et, varius ipsum. Morbi tristique neque quis augue luctus sagittis. Duis tincidunt porta tellus non ultricies. Nulla id diam arcu. Pellentesque elementum scelerisque turpis. Nam varius arcu et dui fringilla gravida a eu tortor. Sed ultricies sem vitae felis varius, vitae maximus ex commodo."
-                   "Prompt #2\nACAACA second prompt"
-                   "Response #2\nACAACA actions"]
-    :actions [actions/pass]}))
-
 (defn init-ai-controller! [^js app]
   (-> (.route app "/ai")
       (.post (fn [req res]
                (let [log (partial log (.toISOString (js/Date.)))]
                  (go (try
                        (let [{:keys [game turn-actions model]} (parse-request req)
-                             {:keys [basic?] :as model} (models-by-id model)
-                             random? (= :random (:id model))
-                             best? (= :best (:id model))
-                             ;; TODO(Richo): Just for testing
-                             actions (cond
-                                       best? (<? (get-best-actions! game turn-actions model log))
-                                       random? (<? (get-random-actions! game turn-actions model log))
-                                       true (<? (get-placeholder-actions! game turn-actions model log))
-                                       basic? (<? (get-basic-actions! game turn-actions model log))
-                                       :else (<? (get-strategy! game turn-actions model log)))]
+                             model (models-by-id model)
+                             get-actions! (handler-by-id (:id model))
+                             actions (<? (get-actions! game turn-actions model log)) ]
                          (doto res
                            (.type "text")
                            (.send (write-response actions))))
